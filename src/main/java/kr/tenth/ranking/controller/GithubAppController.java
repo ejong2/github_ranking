bacaaa60463949f2c789eb3b1ea0977c9baeed33
enamu <guyus5594@gmail.com>
2023-05-01 20:35:29 +0900

    유저 관련 서비스 클래스 분리 #2

--- src/main/java/kr/tenth/ranking/controller/GithubAppController.java
+++ src/main/java/kr/tenth/ranking/controller/GithubAppController.java
@@ -31,7 +31,6 @@ import java.util.*;
 public class GithubAppController {
 
     private final GithubConfig gitHubConfig;
-    private final UserRepository userRepository;
     private final GithubUserService githubUserService;
     private static final String TOKEN_URL = "https://github.com/login/oauth/access_token";
 
@@ -74,11 +73,9 @@ public class GithubAppController {
         HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(requestBody, headers);
         ResponseEntity<String> response = restTemplate.postForEntity(TOKEN_URL, request, String.class);
 
-        // 액세스 토큰을 얻고, 사용자 정보를 저장합니다.
-        ObjectMapper objectMapper = new ObjectMapper();
         JsonNode tokenNode;
         try {
-            tokenNode = objectMapper.readTree(response.getBody());
+            tokenNode = githubUserService.getJsonNode(response.getBody());
         } catch (JsonProcessingException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("액세스 토큰 처리 중 오류가 발생했습니다.");
         }
@@ -89,11 +86,10 @@ public class GithubAppController {
 
         HttpEntity<Void> userRequestEntity = new HttpEntity<>(null, userRequestHeaders);
 
-        // 사용자 정보를 얻어옵니다.
         ResponseEntity<String> userResponse = restTemplate.exchange("https://api.github.com/user", HttpMethod.GET, userRequestEntity, String.class);
         JsonNode userNode;
         try {
-            userNode = objectMapper.readTree(userResponse.getBody());
+            userNode = githubUserService.getJsonNode(userResponse.getBody());
         } catch (JsonProcessingException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("사용자 정보 처리 중 오류가 발생했습니다.");
         }
@@ -102,42 +98,22 @@ public class GithubAppController {
         ResponseEntity<String> orgsResponse = restTemplate.exchange(apiUrl, HttpMethod.GET, userRequestEntity, String.class);
         JsonNode orgsNode;
         try {
-            orgsNode = objectMapper.readTree(orgsResponse.getBody());
+            orgsNode = githubUserService.getJsonNode(orgsResponse.getBody());
         } catch (JsonProcessingException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("조직 정보 처리 중 오류가 발생했습니다.");
         }
 
-        List<Organization> organizations = new ArrayList<>();
-        for (JsonNode orgNode : orgsNode) {
-            String orgName = orgNode.get("login").asText();
-            Organization organization = new Organization(orgName);
-            organizations.add(organization);
-        }
+        List<Organization> organizations = githubUserService.getOrganizations(orgsNode);
 
         String githubUsername = userNode.get("login").asText();
         String profileImageUrl = userNode.get("avatar_url").asText();
 
-        // 깃허브 계정 생성 시점을 얻어옵니다.
         String accountCreatedDateString = userNode.get("created_at").asText();
         ZonedDateTime accountCreatedDateTime = ZonedDateTime.parse(accountCreatedDateString, DateTimeFormatter.ISO_OFFSET_DATE_TIME);
         LocalDate accountCreatedDate = accountCreatedDateTime.toLocalDate();
 
-        // 기존 사용자가 있는지 확인하고, 액세스 토큰을 업데이트합니다.
-        Optional<User> existingUser = userRepository.findByGithubUsername(githubUsername);
-        if (existingUser.isPresent()) {
-            User userToUpdate = existingUser.get();
-            userToUpdate.setAccessToken(accessToken);
-            userRepository.save(userToUpdate);
-        } else {
-            // 새로운 사용자를 저장합니다.
-            User newUser = new User(githubUsername, accessToken, accountCreatedDate, profileImageUrl);
-            for (Organization organization : organizations) {
-                newUser.addOrganization(organization);
-            }
-            userRepository.save(newUser);
-        }
+        User user = githubUserService.saveOrUpdateUser(githubUsername, accessToken, accountCreatedDate, profileImageUrl, organizations);
 
-        // 로그인 성공 페이지로 리디렉션하는 대신 응답에 github_username을 포함합니다.
         Map<String, String> responseBody = new HashMap<>();
         responseBody.put("access_token", accessToken);
         responseBody.put("github_username", githubUsername);
--- src/main/java/kr/tenth/ranking/service/GithubUserService.java
+++ src/main/java/kr/tenth/ranking/service/GithubUserService.java
@@ -1,24 +1,25 @@
 package kr.tenth.ranking.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import kr.tenth.ranking.domain.Organization;
 import kr.tenth.ranking.domain.User;
 import kr.tenth.ranking.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class GithubUserService {
-
     private final UserRepository userRepository;
-
-    public GithubUserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
 
     public User saveOrUpdateUser(String githubUsername, String accessToken, LocalDate accountCreatedDate, String profileImageUrl, List<Organization> organizations) {
         Optional<User> existingUser = userRepository.findByGithubUsername(githubUsername);
@@ -33,5 +34,20 @@ public class GithubUserService {
             }
             return userRepository.save(newUser);
         }
+    }
+
+    public JsonNode getJsonNode(String responseBody) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readTree(responseBody);
+    }
+
+    public List<Organization> getOrganizations(JsonNode orgsNode) {
+        List<Organization> organizations = new ArrayList<>();
+        for (JsonNode orgNode : orgsNode) {
+            String orgName = orgNode.get("login").asText();
+            Organization organization = new Organization(orgName);
+            organizations.add(organization);
+        }
+        return organizations;
     }
 }
